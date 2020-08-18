65a078ddd8772d94157500230262b71b84d1e27c
diana96alazzam <diana-azzam@STDLTUC-L019.localdomain>
2020-08-18 14:39:11 +0300

    waiting status

--- src/App.js
+++ src/App.js
@@ -5,19 +5,8 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header>
+        <img src='https://cdn.dribbble.com/users/3151532/screenshots/5997945/2.jpg' alt='good things take time'/>
       </header>
     </div>
   );
